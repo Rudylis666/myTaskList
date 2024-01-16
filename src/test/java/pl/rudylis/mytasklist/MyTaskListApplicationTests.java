ed358a64f89b47b29188c01898b701015b8a220f
Aleksandra Lasiewicka <alex.lasiewicka@gmail.com>
2024-01-16 08:26:54 +0100

    Initial commit

---
+++ src/test/java/pl/rudylis/mytasklist/MyTaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rudylis.mytasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTaskListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
